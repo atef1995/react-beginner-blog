68364da4e8deed8e4afa508a2e4918ab1a38b91d
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2024-01-22 19:00:47 +0100

    added picture

--- beginners-blog/src/pages/Portfolio/data/ProjectsData.js
+++ beginners-blog/src/pages/Portfolio/data/ProjectsData.js
@@ -1,5 +1,5 @@
 export const ProjectsData = {
-    1:{
+    1: {
         title: 'PDF to GPT Chat Application with Authentication',
         description: `This is a full-stack application that allows users to chat with OpenAI's GPT models. Users can register, login, and then interact with the model via a simple interface. The application uses a React frontend, a Python FastAPI backend, and relies on OpenAI's GPT API for the chat functionality.`,
         prevlink: 'https://www.youtube.com/watch?v=SmfL3-HDa5s',
@@ -7,12 +7,12 @@ export const ProjectsData = {
         image: 'assets/images/gpttopdf.jpg',
     },
 
-    
-    2:{
+
+    2: {
         title: 'Express Book Reviews with JWT Authentication',
         description: `Node.js web application using Express.js for managing and displaying book reviews. It features user registration and login, JWT-based session management, and CRUD operations for book reviews. The application offers endpoints to browse books, and users can add, update, or delete reviews for each book. It's built with a focus on backend functionality, demonstrating proficiency in server-side JavaScript, RESTful API design, and user authentication.`,
         prevlink: 'https://github.com/atef1995/expressBookReviews',
         github: 'https://github.com/atef1995/expressBookReviews',
-        image: '',
+        image: 'assets/images/7-login.png',
     }
 }
